ae3674f91b1f147ee09ed31f4bb14aa9c18eaaf7
AB-tachyonwinds <agbautista1022@gmail.com>
2024-11-01 19:47:39 -0700

    runs start:dev concurrently with build:dev

--- team-qwirkle/server/index.ts
+++ team-qwirkle/server/index.ts
@@ -5,6 +5,12 @@ import httpErrors from "http-errors";
 import morgan from "morgan";
 import * as path from "path";
 import rootRoutes from "./routes/root";
+
+import connectLiveReload from "connect-livereload";
+import livereload from "livereload";
+
+require("dotenv").config();
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,3 +28,20 @@ app.use((_request, _response, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const staticPath = path.join(process.cwd(), "src", "public");
+app.use(express.static(staticPath));
+console.log("working\n");
+
+if (process.env.MODES === "development") {
+  const reloadServer = livereload.createServer();
+  reloadServer.watch(staticPath);
+  reloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      reloadServer.refresh("/");
+    }, 100);
+  });
+  app.use(connectLiveReload());
+} else {
+  console.log("broken\n");
+}
