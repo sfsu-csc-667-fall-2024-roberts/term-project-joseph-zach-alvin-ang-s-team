d0fdcc7ff31923a649189f48ea9653282c5e4394
AB-tachyonwinds <agbautista1022@gmail.com>
2024-12-13 14:03:16 -0800

    updated lobby.ejs

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+/* import { io, Socket } from "socket.io-client";
+
+declare global {
+  interface Window {
+    socket: Socket;
+    roomId: number;
+  }
+}
+
+// declare module "express-session" {
+//   interface SessionData {
+//     user: {
+//       id: number;
+//       username: string;
+//       email: string;
+//       gravatar: string;
+//     };
+//     roomId: number;
+//   }
+// }
+
+window.socket = io();
+*/
