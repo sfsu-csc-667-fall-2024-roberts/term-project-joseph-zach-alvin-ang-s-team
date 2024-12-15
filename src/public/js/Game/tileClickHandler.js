9e8ff9af413bde8f2f1003f9f3e4bc5a28f60ea3
Alvin-Tulud <alvintulud@gmail.com>
2024-12-14 16:38:50 -0800

    game page stuff

---
+++ src/public/js/Game/tileClickHandler.js
@@ -0,0 +1,34 @@
+const tiles = document.getElementsByClassName("tile");
+const tile_slots = document.getElementsByClassName("tile-spot");
+const tilesArr = Array.from(tiles);
+const slotsArr = Array.from(tile_slots);
+
+let tileSelected = false;
+let tileObj;
+
+var click_count_tile = 0;
+var click_count_slot = 0;
+
+tilesArr.forEach((clickTile) => {
+  clickTile.addEventListener("mousedown", (e) => {
+    click_count_tile++;
+    console.log("clicked tile: " + click_count_tile);
+
+    tileSelected = true;
+    tileObj = e;
+  });
+});
+
+slotsArr.forEach((clickSlot) => {
+  clickSlot.addEventListener("mousedown", (e) => {
+    console.log("clicked tile_spot: " + clickSlot.dataset.coord);
+    console.log("clicked tile_spot: " + clickSlot.dataset.hasTile);
+
+    if (tileSelected) {
+      tileSelected = false;
+
+      console.log("attempt placing at: " + clickSlot.dataset.coord);
+      console.log("tile to be placed: " + tileObj.src);
+    }
+  });
+});
--- team-qwirkle/server/index.ts
+++ team-qwirkle/server/index.ts
@@ -24,6 +24,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(process.cwd(), "src", "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 //app.use(flash());
 //app.use(session());
