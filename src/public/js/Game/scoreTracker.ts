e3d70b86eabcd6e72cfe037a16702c866d2c9b83
Alvin-Tulud <alvintulud@gmail.com>
2024-12-16 22:03:39 -0800

    added some of his queries that we might be able to use

---
+++ src/public/js/Game/scoreTracker.ts
@@ -0,0 +1,1 @@
+const Scoreboard = document.getElementsByClassName("scoreboard");
--- team-qwirkle/server/db/Game/sql.ts
+++ team-qwirkle/server/db/Game/sql.ts
@@ -7,3 +7,29 @@ RETURNING account_id, player_id, username, password, created
 export const FIND_BY_USERNAME_SQL = `
 SELECT * FROM account WHERE username = $1
 `;
+
+export const CREATE_GAME = `
+INSERT INTO games DEFAULT VALUES RETURNING *, 1 as players
+`;
+
+export const ADD_PLAYER = `
+INSERT INTO game_users (game_id, user_id, seat)
+VALUES ($1, $2, (SELECT COUNT(*) FROM game_users WHERE game_id = $1) + 1)
+RETURNING 
+  game_id AS id, 
+  (SELECT COUNT(*) FROM game_users WHERE game_id = $1) AS players,
+  (SELECT player_count FROM games WHERE id = $1) AS player_count
+`;
+
+export const AVAILABLE_GAMES = `
+SELECT *, 
+  (SELECT COUNT(*) FROM game_users WHERE games.id=game_users.game_id) AS players 
+FROM games WHERE id IN 
+  (SELECT game_id FROM game_users GROUP BY game_id HAVING COUNT(*) < 4)
+LIMIT $1
+OFFSET $2
+`;
+
+export const GET_PLAYER_COUNT = `
+  SELECT COUNT(*) FROM game_users WHERE game_id = $1
+`;
